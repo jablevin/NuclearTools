4a3865934d4dcc0c4ddefc0f261d5289ba539e69
Jacob Blevins <jablevin@wolftech.ad.ncsu.edu>
2020-03-03 09:43:29 -0500

    Reformating layout

--- setup_local.py
+++ setup_local.py
@@ -6,14 +6,23 @@ pythonlib = pythonlib.replace('\\', '/')
 cwd = os.getcwd()
 cwd = cwd.replace('\\', '/')
 
-newpath = pythonlib + '/NuclearTools'
+newpath = pythonlib
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
+# try:
+#     # print('cp \'' + cwd + '/NuclearTools/Tools.py\' ' + newpath)
+#     os.system('cp \'' + cwd + '/NuclearTools/Tools.py\' ' + newpath)
+#     os.system('cp \'' + cwd + '/NuclearTools/Nuclide_Data.txt\' ' + newpath)
+#     os.system('cp \'' + cwd + '/NuclearTools/__init__.py\' ' + newpath)
+#     os.system('cp \'' + cwd + '/NuclearTools/ThermalHydraulics.py\' ' + newpath)
+#     os.system('cp \'' + cwd + '/NuclearTools/MassFlux.py\' ' + newpath)
+#     os.system('cp \'' + cwd + '/NuclearTools/Natural_Circulation_SMR.py\' ' + newpath)
+# except:
+#     pass
+
 try:
-    os.system('cp \'' + cwd + '/NuclearTools/Tools.py\' ' + newpath)
-    os.system('cp \'' + cwd + '/NuclearTools/Nuclide_Data.txt\' ' + newpath)
-    os.system('cp \'' + cwd + '/NuclearTools/__init__.py\' ' + newpath)
-    os.system('cp \'' + cwd + '/NuclearTools/ThermalHydraulics.py\' ' + newpath)
+    os.system('cp -r \'' + cwd + '/build/lib.win-amd64-3.7/NuclearTools\' ' + newpath + '/')
+    # print('cp -r \'' + cwd + '/build/lib.win-amd64-3.7/NuclearTools\' ' + newpath + '/')
 except:
     pass
